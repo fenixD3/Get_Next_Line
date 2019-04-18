447135f59de0a1c45e8cc17690cae6749a30a96a
Yeste Lila <ylila@oa-i2.21-school.ru>
2019-04-18 15:40:18 +0300

    For 1 file desc work well

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ylila <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 13:31:29 by ylila             #+#    #+#             */
-/*   Updated: 2019/04/18 12:47:37 by ylila            ###   ########.fr       */
+/*   Updated: 2019/04/18 15:39:03 by ylila            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,75 +16,27 @@
 
 #include <stdio.h>
 
-/*static size_t	check_lb(const char *buff, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (size--)
-	{
-		if (buff[i] == '\n')
-			return (i);
-		++i;
-	}
-	return (0);
-}
-
 int				get_next_line(const int fd, char **line)
 {
-	char		buff[BUFF_SIZE];
+	char		buf[BUFF_SIZE + 1];
 	static char	*strs;
-	ssize_t		ret;
-	size_t		ind_len;
+	char		*lb;
+	ssize_t		b;
 
-	if (fd == -1 || !line || !*line)
+	if ((fd < 0 || !line || !*line) || (!strs && !(strs = ft_strnew(0))))
 		return (-1);
-	while ((ret = read(fd, &buff, BUFF_SIZE)) > 0)
+	while (!(lb = ft_strchr(strs, '\n')) && (b = read(fd, &buf, BUFF_SIZE)) > 0)
 	{
-		if (!(ind_len = check_lb(buff, ret)))
-			*line = ft_strncat(*line, buff, ret);
-		else
-		{
-			*line = ft_strncat(*line, buff, ind_len);
-			return (1);
-		}
+		buf[b] = '\0';
+		strs = ft_strjoin(strs, buf);
 	}
-	return (0);
-}*/
-
-int				get_next_line(const int fd, char **line)
-{
-	char		buff[BUFF_SIZE + 1];
-	static char	*strs;
-	char		*final;
-	ssize_t		bytes;
-
-	if ((fd < 0 || !line || !*line) || (strs == NULL && !(strs = ft_strnew(0))))
-		return (-1);
-	while (!ft_strchr(strs, '\n') && (bytes = read(fd, &buff, BUFF_SIZE)) > 0)
+	if (b == 0)
 	{
-		buff[bytes] = '\0';
-		if (!ft_strchr(buff, '\n'))
-			strs = ft_strjoin(strs, buff);
-		else
-		{
-			final = ft_strsub(buff, 0, ft_strchr(buff, '\n') - buff);
-			strs = ft_strjoin(strs, final);
-			*line = ft_strsub(strs, 0, ft_strlen(strs));
-			strs = ft_strsub(buff, (unsigned int)(ft_strlen(final) + 1),
-					ft_strlen(buff) - ft_strlen(final));
-			printf("--------------------------------\n");
-			printf("Strs: %s - Final: %s\n", strs, final);
-			return (1);
-		}
+		ft_strdel(&strs);
+		return (0);
 	}
-	printf("--------------------------------\n");
-	printf("Strs before: %s\n", strs);
-	*line = ft_strsub(strs, 0, ft_strchr(strs, '\n') - strs);
+	*line = ft_strsub(strs, 0, lb - strs);
 	strs = ft_strsub(strs, (unsigned int)(ft_strlen(*line) + 1),
 			ft_strlen(strs) - ft_strlen(*line));
-	printf("Strs: %s\n", strs);
-	if (!strs)
-		return (0);
 	return (1);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ylila <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 12:55:59 by ylila             #+#    #+#             */
-/*   Updated: 2019/04/17 16:02:36 by ylila            ###   ########.fr       */
+/*   Updated: 2019/04/18 14:27:05 by ylila            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,22 @@
 int		main(void)
 {
 	int fd = open("test.txt", O_RDONLY);
+	int fd1 = open("test1.txt", O_RDONLY);
 	int gnl_error;
 	char *line = ft_strnew(255);
 
 	while ((gnl_error = get_next_line(fd, &line)) == 1)
 		printf("%s\n", line);
 	/*get_next_line(fd, &line);
+	printf("%s\n", line);
+
+	get_next_line(fd1, &line);
+	printf("%s\n", line);
+
+	get_next_line(fd, &line);
 	printf("%s\n", line);*/
 
 	close(fd);
+	close(fd1);
 	return (0);
 }
